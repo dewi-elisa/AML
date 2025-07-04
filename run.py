2a3a8fda313a2de8e90150de7e6a869c4306b337
Dewi-Elisa <dewi.timman@gmail.com>
2025-07-04 16:14:20 +0200

    make the code not run when run.py is called

--- run.py
+++ run.py
@@ -10,73 +10,6 @@ from core.models.model_factory import Model
 from core.utils import preprocess
 import core.trainer as trainer
 
-# -----------------------------------------------------------------------------
-parser = argparse.ArgumentParser(description='PyTorch video prediction model - PredRNN')
-
-# training/test
-parser.add_argument('--is_training', type=int, default=1)
-parser.add_argument('--device', type=str, default='cpu:0')
-
-# data
-parser.add_argument('--dataset_name', type=str, default='mnist')
-parser.add_argument('--train_data_paths', type=str, default='data/moving-mnist-example/moving-mnist-train.npz')
-parser.add_argument('--valid_data_paths', type=str, default='data/moving-mnist-example/moving-mnist-valid.npz')
-parser.add_argument('--save_dir', type=str, default='checkpoints/mnist_predrnn')
-parser.add_argument('--gen_frm_dir', type=str, default='results/mnist_predrnn')
-parser.add_argument('--input_length', type=int, default=10)
-parser.add_argument('--total_length', type=int, default=20)
-parser.add_argument('--img_width', type=int, default=64)
-parser.add_argument('--img_channel', type=int, default=1)
-parser.add_argument('--noise', type=str, default=None)
-parser.add_argument('--noise_ratio', type=float, default=0.1)
-parser.add_argument('--noise_size', type=int, default=10)
-
-# model
-parser.add_argument('--model_name', type=str, default='predrnn')
-parser.add_argument('--pretrained_model', type=str, default='')
-parser.add_argument('--num_hidden', type=str, default='64,64,64,64')
-parser.add_argument('--filter_size', type=int, default=5)
-parser.add_argument('--stride', type=int, default=1)
-parser.add_argument('--patch_size', type=int, default=4)
-parser.add_argument('--layer_norm', type=int, default=1)
-parser.add_argument('--decouple_beta', type=float, default=0.1)
-
-# reverse scheduled sampling
-parser.add_argument('--reverse_scheduled_sampling', type=int, default=0)
-parser.add_argument('--r_sampling_step_1', type=float, default=25000)
-parser.add_argument('--r_sampling_step_2', type=int, default=50000)
-parser.add_argument('--r_exp_alpha', type=int, default=5000)
-# scheduled sampling
-parser.add_argument('--scheduled_sampling', type=int, default=1)
-parser.add_argument('--sampling_stop_iter', type=int, default=50000)
-parser.add_argument('--sampling_start_value', type=float, default=1.0)
-parser.add_argument('--sampling_changing_rate', type=float, default=0.00002)
-
-# optimization
-parser.add_argument('--lr', type=float, default=0.001)
-parser.add_argument('--reverse_input', type=int, default=1)
-parser.add_argument('--batch_size', type=int, default=8)
-parser.add_argument('--max_iterations', type=int, default=80000)
-parser.add_argument('--display_interval', type=int, default=100)
-parser.add_argument('--test_interval', type=int, default=5000)
-parser.add_argument('--snapshot_interval', type=int, default=5000)
-parser.add_argument('--num_save_samples', type=int, default=10)
-parser.add_argument('--n_gpu', type=int, default=1)
-
-# visualization of memory decoupling
-parser.add_argument('--visual', type=int, default=0)
-parser.add_argument('--visual_path', type=str, default='./decoupling_visual')
-
-# action-based predrnn
-parser.add_argument('--injection_action', type=str, default='concat')
-parser.add_argument('--conv_on_input', type=int, default=0, help='conv on input')
-parser.add_argument('--res_on_conv', type=int, default=0, help='res on conv')
-parser.add_argument('--num_action_ch', type=int, default=4, help='num action ch')
-
-args = parser.parse_args()
-print(args)
-
-
 def reserve_schedule_sampling_exp(itr):
     if itr < args.r_sampling_step_1:
         r_eta = 0.5
@@ -212,19 +145,85 @@ def test_wrapper(model):
     trainer.test(model, test_input_handle, args, 'test_result')
 
 
-if os.path.exists(args.save_dir):
-    shutil.rmtree(args.save_dir)
-os.makedirs(args.save_dir)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='PyTorch video prediction model - PredRNN')
 
-if os.path.exists(args.gen_frm_dir):
-    shutil.rmtree(args.gen_frm_dir)
-os.makedirs(args.gen_frm_dir)
+    # training/test
+    parser.add_argument('--is_training', type=int, default=1)
+    parser.add_argument('--device', type=str, default='cpu:0')
 
-print('Initializing models')
+    # data
+    parser.add_argument('--dataset_name', type=str, default='mnist')
+    parser.add_argument('--train_data_paths', type=str, default='data/moving-mnist-example/moving-mnist-train.npz')
+    parser.add_argument('--valid_data_paths', type=str, default='data/moving-mnist-example/moving-mnist-valid.npz')
+    parser.add_argument('--save_dir', type=str, default='checkpoints/mnist_predrnn')
+    parser.add_argument('--gen_frm_dir', type=str, default='results/mnist_predrnn')
+    parser.add_argument('--input_length', type=int, default=10)
+    parser.add_argument('--total_length', type=int, default=20)
+    parser.add_argument('--img_width', type=int, default=64)
+    parser.add_argument('--img_channel', type=int, default=1)
+    parser.add_argument('--noise', type=str, default=None)
+    parser.add_argument('--noise_ratio', type=float, default=0.1)
+    parser.add_argument('--noise_size', type=int, default=10)
 
-model = Model(args)
+    # model
+    parser.add_argument('--model_name', type=str, default='predrnn')
+    parser.add_argument('--pretrained_model', type=str, default='')
+    parser.add_argument('--num_hidden', type=str, default='64,64,64,64')
+    parser.add_argument('--filter_size', type=int, default=5)
+    parser.add_argument('--stride', type=int, default=1)
+    parser.add_argument('--patch_size', type=int, default=4)
+    parser.add_argument('--layer_norm', type=int, default=1)
+    parser.add_argument('--decouple_beta', type=float, default=0.1)
 
-if args.is_training:
-    train_wrapper(model)
-else:
-    test_wrapper(model)
+    # reverse scheduled sampling
+    parser.add_argument('--reverse_scheduled_sampling', type=int, default=0)
+    parser.add_argument('--r_sampling_step_1', type=float, default=25000)
+    parser.add_argument('--r_sampling_step_2', type=int, default=50000)
+    parser.add_argument('--r_exp_alpha', type=int, default=5000)
+    # scheduled sampling
+    parser.add_argument('--scheduled_sampling', type=int, default=1)
+    parser.add_argument('--sampling_stop_iter', type=int, default=50000)
+    parser.add_argument('--sampling_start_value', type=float, default=1.0)
+    parser.add_argument('--sampling_changing_rate', type=float, default=0.00002)
+
+    # optimization
+    parser.add_argument('--lr', type=float, default=0.001)
+    parser.add_argument('--reverse_input', type=int, default=1)
+    parser.add_argument('--batch_size', type=int, default=8)
+    parser.add_argument('--max_iterations', type=int, default=80000)
+    parser.add_argument('--display_interval', type=int, default=100)
+    parser.add_argument('--test_interval', type=int, default=5000)
+    parser.add_argument('--snapshot_interval', type=int, default=5000)
+    parser.add_argument('--num_save_samples', type=int, default=10)
+    parser.add_argument('--n_gpu', type=int, default=1)
+
+    # visualization of memory decoupling
+    parser.add_argument('--visual', type=int, default=0)
+    parser.add_argument('--visual_path', type=str, default='./decoupling_visual')
+
+    # action-based predrnn
+    parser.add_argument('--injection_action', type=str, default='concat')
+    parser.add_argument('--conv_on_input', type=int, default=0, help='conv on input')
+    parser.add_argument('--res_on_conv', type=int, default=0, help='res on conv')
+    parser.add_argument('--num_action_ch', type=int, default=4, help='num action ch')
+
+    args = parser.parse_args()
+    print(args)
+
+    if os.path.exists(args.save_dir):
+        shutil.rmtree(args.save_dir)
+    os.makedirs(args.save_dir)
+
+    if os.path.exists(args.gen_frm_dir):
+        shutil.rmtree(args.gen_frm_dir)
+    os.makedirs(args.gen_frm_dir)
+
+    print('Initializing models')
+
+    model = Model(args)
+
+    if args.is_training:
+        train_wrapper(model)
+    else:
+        test_wrapper(model)
